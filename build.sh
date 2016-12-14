0a881922c166a6bf68ada265d3e34e54f40096b4
Chinple <happyheartlong@qq.com>
2016-12-14 16:18:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ rm */*.pyc
 rm */*/*.pyc
 
 rm ../mserver.tar
-tar cf ../mserver.tar run.py  mtest.py cserver.py cperf.py libs db server tmodel
+tar cf ../mserver.tar run.py  mtest.py cserver.py cperf.py libs db server tmodel tools
 ls -l ../mserver.tar
