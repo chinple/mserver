59378c7b22c03219aa7a675c04ce08277e9f6145
Chinple <happyheartlong@qq.com>
2017-02-28 14:33:14 +0800

    Update syscmd.py

--- tools/cmd/syscmd.py
+++ tools/cmd/syscmd.py
@@ -38,7 +38,25 @@ class CmdExecuter:
             return self.cmdres
         return "> %s\n%s\n%s%s" % (self.cmdstr, self.retcode, self.cmdres, self.cmderr)
 
-
+def replaceIniconfig(toIniFile, fromIniFile):
+    import codecs
+    with codecs.open(fromIniFile, encoding="utf-8") as iniFile:
+        while True:
+            l = iniFile.readline()
+            if l == '':
+                break
+            l = l.strip()
+            try:
+                c0 = l[0]
+                if c0 == '#' or c0 == ";":continue
+                i = l.index('=')
+                n, v = l[0:i].strip(), l[i + 1:].strip()
+                if n == "":continue
+            except:continue
+            v = v.replace("'", "''")
+            cmd = "sed -i 's/%s.*=.*/%s=%s/g' %s" % (n, n, v, toIniFile)
+            print cmd
+            os.system(cmd)
 
 # p = subprocess.Popen(["ssh -l root -p 36000 183.60.82.9 hostname"],
 #     stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
