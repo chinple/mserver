c16fac10d5151522deb727002583b16e676d9aab
Chinple <happyheartlong@qq.com>
2017-02-28 14:35:39 +0800

    Create logProxy.py

---
+++ tools/stool/logProxy.py
@@ -0,0 +1,18 @@
+'''
+Created on 2016-8-29
+
+@author: chinple
+'''
+from server.cproxy import LogHttpHandle
+from cserver import cloudModule
+
+@cloudModule(handleUrl="/")
+class LogHttpProxy(LogHttpHandle):
+    def __analyzeSession__(self, isMock, isPost, reqPath, reqParam, respBody,
+            reqTime, respTime, respStatus, reqAddress, reqHeader, respHeader):
+        self.getHostLog("requestHeader").info(str(dict(reqHeader)))
+        LogHttpHandle.__analyzeSession__(self, isMock, isPost, reqPath, reqParam, respBody, reqTime, respTime, respStatus, reqAddress, reqHeader, respHeader)
+
+if __name__ == "__main__":
+    from cserver import servering
+    servering(" -p 8080")
