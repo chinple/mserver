c90f5c3d5a254ef5f1ee0396251473dbd86d5ecb
Chinple <happyheartlong@qq.com>
2017-06-06 13:57:31 +0800

    Create report.h

---
+++ test/libstest/report.h
@@ -0,0 +1,43 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>report</title>
+<style type="text/css">
+<!--
+/************ Table ************/
+.cplantable {border-collapse: collapse;border: 1px solid #ccc;}    
+.cplantable thead td {font-size: 14px;color: #333333;text-align: center;background: repeat-x top center;border: 1px solid #ccc; font-weight:bold;}
+.cplantable tbody tr {background: #fff;font-size: 12px;color: #666666;}           
+.cplantable tbody tr.alt-row {background: #f2f7fc;}               
+.cplantable td{line-height:20px;text-align: left;padding:4px 10px 3px 10px;height: 18px;border: 1px solid #ccc;}
+
+textarea {
+-moz-box-shadow:0 0 0 #E7E7E7;
+-moz-box-sizing:border-box;
+border-color:#CCCCCC #999999 #999999 #CCCCCC;
+border-style:solid;
+border-width:0px;
+font-family:arial,sans-serif;
+font-size:12px;
+height:100px;
+margin:0px auto;
+outline-color:-moz-use-text-color;
+outline-style:none;
+outline-width:medium;
+padding:0px;
+width:300px;
+}
+-->
+</style>
+</head>
+
+<body>
+<table class="cplantable" width=1500>
+<thead> <td>名称 </td> <td>状态 </td> <td>进度</td> <td>更新日期</td> <td>计划详情</td> <td>问题或风险</td> <td>最新进度</td> </thead>
+<tbody>
+	{plans}
+</tbody>
+</table>
+
+</body>
+</html>
