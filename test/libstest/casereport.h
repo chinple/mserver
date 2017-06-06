87550e86750a9dbb66da84e18180d2c945c6850b
Chinple <happyheartlong@qq.com>
2017-06-06 13:59:04 +0800

    Create casereport.h

---
+++ test/libstest/casereport.h
@@ -0,0 +1,25 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>testcases</title>
+<style type="text/css">
+<!--
+.cplantable {border-collapse: collapse;border: 1px solid #ccc;}    
+.cplantable thead td {font-size: 14px;color: #333333;text-align: center;background: repeat-x top center;border: 1px solid #ccc; font-weight:bold;}
+.cplantable tbody tr {background: #fff;font-size: 12px;color: #666666;}           
+.cplantable tbody tr.alt-row {background: #f2f7fc;}               
+.cplantable td{line-height:20px;text-align: left;padding:4px 10px 3px 10px;height: 18px;border: 1px solid #ccc;}
+-->
+</style>
+</head>
+
+<body>
+<table class="cplantable">
+<thead> <td>测试场景 </td> <td width=600>测试要点 </td> <td width=50>优先级</td> <td>Owner</td> <td>备注</td>  </thead>
+<tbody>
+	{testcases}
+</tbody>
+</table>
+
+</body>
+</html>
